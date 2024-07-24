2ed54791e0b01787fb64a1a676618713bb48b63a
Jady Godoi <jady.godoi@oi.net.br>
2024-07-24 16:22:47 -0300

    feat: comentarios com explicações e contextos para estudo

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
+/*
+é um middleware que permite que o Express analise dados codificados em URL (como os dados enviados de um formulário HTML). O { extended: true } permite que objetos aninhados sejam analisados corretamente.
+*/
+
 
 app.get('/', (request, response) => {
     response.send(`
@@ -13,12 +17,19 @@ app.get('/', (request, response) => {
 })
 
 app.get('/testes/:idUsuarios?/:perfil?', (request, response) => {
-    console.log(request.params)
-    console.log(request.query)
+    /*
+    define uma rota que pode receber parâmetros de rota opcionais (idUsuarios e perfil). O ponto de interrogação ? após o nome do parâmetro indica que ele é opcional.
+    */
+    console.log(request.params) //contém os parâmetros de rota enviados.
+    console.log(request.query) //ontém os parâmetros de query string (dados enviados na URL após o ?).
     response.send('Oiiiii')
 })
 
 app.post('/', (request, response) => {
+    /*
+    app.post('/', (request, response) => { ... }); define uma rota que processa os dados enviados via POST para a página principal (/).
+    request.body contém os dados do formulário enviados no corpo da solicitação.
+    */
     console.log(request.body)
     response.send(`O que recebi do formulário foi: ${request.body.nome}`)
 })
@@ -27,7 +38,7 @@ app.get('/contato', (request, response) => {
     response.send('Obrigado por entrar em contato com a gente.')
 })
 
-app.listen(3000, () => {
+app.listen(3000, () => { //faz com que o aplicativo comece a escutar conexões na porta 3000.
     console.log('http://localhost:3000')
     console.log('servidor executando na porta 3000')
 })
